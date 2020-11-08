656663fc60f17fe8a16828644a8c9eb16f1bc53c
Ryan Mahoney <ryan.mahoney@outlook.com>
2020-11-08 18:29:02 -0500

    remove  typed function, update deps (#3)

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,8 @@
 "use strict";
 
-var _objectmodel = require("objectmodel");
-
-var FunctionModel = _objectmodel.FunctionModel;
-var Model = _objectmodel.Model;
-
-
 const handleReturnValue = (value, prevState) => typeof value === "object" ? [true, Object.assign({}, prevState, value)] : [value === false ? false : true, prevState];
 
-const callFunction = async (fn, state) => handleReturnValue((await fn(state)), state);
+const callFunction = async (fn, state) => handleReturnValue(await fn(state), state);
 
 const every = exports.every = async (functions, initialState = {}) => {
   const finalState = await functions.reduce(async (previousPromise, fn) => {
@@ -17,5 +11,3 @@ const every = exports.every = async (functions, initialState = {}) => {
   }, Promise.resolve([true, initialState]));
   return finalState[1];
 };
-
-const typed = exports.typed = (typeIn, typeOut, fn) => fn === undefined ? FunctionModel(Model(typeIn))(typeOut) : FunctionModel(Model(typeIn)).return(Model(typeOut))(fn);
